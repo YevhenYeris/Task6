2fe85fb009c4e538876a9c143ee506a109186da2
YevhenYeris <58602923+YevhenYeris@users.noreply.github.com>
2020-11-10 17:21:23 +0200

    Update Program.cs

--- NovorichnaYolka/Program.cs
+++ NovorichnaYolka/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 
+//ЯЛИНКА
+
 namespace NovorichnaYolka
 {
     class Program
